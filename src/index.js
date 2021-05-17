ca29a337e3843608cfa34f927e199fba348fd33c
bsak350 <bsak350@gmail.com>
2021-05-16 18:22:00 -0700

    Imported bootstrap css to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootsrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
 
 import "./index.css";
@@ -8,17 +8,19 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <Weather />
-      <p>
-        <a
-          href="https://github.com/bsak350/weather-react.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Brittany Sak
-      </p>
+      <div className="container">
+        <Weather />
+        <p>
+          <a
+            href="https://github.com/bsak350/weather-react.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Brittany Sak
+        </p>
+      </div>
     </div>
   );
 }
