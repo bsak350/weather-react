68b242de56f27d3a429508042e76308c4742c1d7
bsak350 <bsak350@gmail.com>
2021-05-25 13:33:07 -0700

    Removed Celsius conversion

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,48 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemp(props) {
-  const [unit, setUnit] = useState("imperial");
-
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("imperial");
-  }
-
-  function celsius() {
-    return (props.imperial - 32) * (5 / 9);
-  }
-
-  if (unit === "imperial") {
-    return (
-      <div className="WeatherTemp">
-        <span className="temp">{props.imperial}</span>
-        <span className="unit">
-          {" "}
-          °F | °
-          <a href="/" onClick={convertToCelsius}>
-            C
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemp">
-        <span className="temp">{Math.round(celsius())}</span>
-        <span className="unit">
-          {" "}
-          °
-          <a href="/" onClick={convertToFahrenheit}>
-            F
-          </a>{" "}
-          | °C
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemp">
+      <span className="temp">{props.imperial}</span>
+      <span className="unit"> °F </span>
+    </div>
+  );
 }
