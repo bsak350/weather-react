7ee50ecb448994398d77643f179f25876bcd6617
bsak350 <bsak350@gmail.com>
2021-05-25 12:47:25 -0700

    Added WeatherForecastDay.js component for 1 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,33 +1,37 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "c6ae7a266fd79d65419f44e508f65fae";
-  let longitude = props.coord.lon;
-  let latitude = props.coord.lat;
-  let units = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <div className="WeatherForecast-icon">☀️</div>
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temp-max">75° </span>
-            <span className="WeatherForecast-temp-min">60° </span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "c6ae7a266fd79d65419f44e508f65fae";
+    let longitude = props.coord.lon;
+    let latitude = props.coord.lat;
+    let units = "imperial";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
